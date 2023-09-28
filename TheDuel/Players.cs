9204774ff2b5dfebeb98b1c2aa98efc22c08f3f0
Broscan <Calle992015@gmail.com>
2023-09-28 12:26:37 +0200

    Initial commit

---
+++ TheDuel/Players.cs
@@ -0,0 +1,36 @@
+﻿namespace TheDuel
+{
+    internal class Player
+    {
+
+        public int Health { get; set; }
+
+        public int Damage { get; set; }
+
+        public Player(int health, int damage)
+        {
+            Health = health;
+            Damage = damage;
+        }
+
+        public int RollDamage()
+        {
+            Random randomDamage = new Random();
+
+            randomDamage.Next(0, 25);
+
+            return randomDamage.Next(0, 25);
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
---
+++ TheDuel/Program.cs
@@ -0,0 +1,82 @@
+﻿using TheDuel;
+
+Player playerOne = new(200, 0);
+
+Player playerTwo = new(200, 0);
+
+Random whichPlayerStarts = new();
+
+bool playerOneStarts = whichPlayerStarts.Next(2) == 0;
+
+while (playerOne.Health > 0 && playerTwo.Health > 0)
+{
+    if (playerOneStarts)
+    {
+        Console.Clear();
+        Console.WriteLine("Player One starts:");
+        Console.WriteLine("Press any key to attack...");
+        Console.ReadKey();
+
+        int damageDone = Attack(playerOne, playerTwo);
+
+        Console.WriteLine($"Player One hit Player Two for {damageDone}");
+        Console.WriteLine($"Player Two has {playerTwo.Health} health remaining");
+        PressKey();
+
+
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Player Two starts:");
+        Console.WriteLine("Press any key to attack...");
+        Console.ReadKey();
+
+        int damageDone = Attack(playerTwo, playerOne);
+
+        Console.WriteLine($"Player Two hit Player One for {damageDone}");
+
+        Console.WriteLine($"Player One has {playerOne.Health} health remaining");
+        PressKey();
+
+    }
+
+
+    playerOneStarts = !playerOneStarts;
+}
+
+if (playerOne.Health < 0)
+{
+    Console.WriteLine("Player Two has won!");
+    PressKey();
+}
+else
+{
+    Console.WriteLine("Player One has won!");
+    PressKey();
+
+}
+
+
+int Attack(Player attackingPlayer, Player attackedPlayer)
+{
+    int damage = attackingPlayer.RollDamage();
+
+    attackedPlayer.Health -= damage;
+
+    return damage;
+}
+
+void PressKey()
+{
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+
+//int CalculateRemainingHealth(Player attackedPlayer);
+//{
+//    int remainingHealth = attackedPlayer.Health;
+
+//    return remainingHealth;
+//}
